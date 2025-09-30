5e39cb005aa84b75af34eedd7280b50c3cb6a867
yanisahq <yahaqar@gmail.com>
2025-09-30 19:51:04 +0200

    maj gaulois

---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,13 @@
+package test_fonctionnel;
+
+import personnage.Gaulois;
+
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		Gaulois obelix = new Gaulois("Obélix",16);
+		
+		
+	}
+
+}
