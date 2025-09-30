6b865687cc4d059ce56f80f5edb5b35ab122c4a5
yanisahq <yahaqar@gmail.com>
2025-09-30 19:27:39 +0200

    création

---
+++ src/personnage/Gaulois.java
@@ -0,0 +1,23 @@
+package personnage;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+}
---
+++ src/personnage/Romain.java
@@ -0,0 +1,23 @@
+package personnage;
+
+public class Romain {
+	private String nom;
+	private int force;
+
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+}
